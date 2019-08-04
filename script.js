b342e8d54b5a28c50c31dde922171671e79dde55
Ghiță Bizău <ghita_bizau@yahoo.com>
2019-08-04 16:45:34 +0300

    WIP clear.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 var btn_save = document.getElementById('btn-save');
+var btn_clear = document.getElementById('btn-clear');
 
 btn_save.onclick = function() {
   alert("Saved");
+  return false;
+};
+
+btn_clear.onclick = function() {
+  alert("Clear");
   return false;
 };
