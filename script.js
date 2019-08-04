0625f849cf8256ae68cba12aa15b587ae361a7b1
Ghiță Bizău <ghita_bizau@yahoo.com>
2019-08-04 17:03:15 +0300

    Save text in local storage.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 var btn_save = document.getElementById('btn-save');
 var btn_clear = document.getElementById('btn-clear');
 var div_notes = document.getElementById('notes');
+var key_local_storage = "quick-notes";
+var saved_text = localStorage.getItem(key_local_storage);
+
+div_notes.innerHTML = saved_text;
 
 btn_save.onclick = function() {
-  alert("Saved");
+  localStorage.setItem(
+    key_local_storage,
+    div_notes.innerHTML);
   return false;
 };
 
